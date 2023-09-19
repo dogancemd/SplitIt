e21e832d32fd0f15eb4f0b9e8b0d16a20e6ab527
Dogancem Duran <dogancemduran@gmail.com>
2023-09-19 22:01:56 +0300

    initial

---
+++ src/main/java/com/splitit/splitit/SplitItApplication.java
@@ -0,0 +1,13 @@
+package com.splitit.splitit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/splitit/splitit/SplitItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitit.splitit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

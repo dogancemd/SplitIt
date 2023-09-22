d7d7ff4cd36b434e8c59c25126a915e169fcbc6a
Dogancem Duran <dogancemduran@gmail.com>
2023-09-22 12:26:09 +0300

    changes

--- src/main/java/com/splitit/splitit/DebtGraph.java
+++ src/main/java/com/splitit/splitit/DebtGraph.java
@@ -15,39 +15,39 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DebtGraph {
-    private HashMap<ObjectId,DebtGraphUser> DebtGraphUsers = new HashMap<>();
+    private HashMap<ObjectId,DebtGraphUser> users = new HashMap<>();
 
     @Id
     private ObjectId groupId;
     public void transaction(ObjectId DebtGraphUser_to_give, ObjectId DebtGraphUser_to_take, long money){
-        DebtGraphUsers.get(DebtGraphUser_to_give).giveMoney(DebtGraphUser_to_take,money);
-        DebtGraphUsers.get(DebtGraphUser_to_take).takeMoney(DebtGraphUser_to_give,money);
+        users.get(DebtGraphUser_to_give).giveMoney(DebtGraphUser_to_take,money);
+        users.get(DebtGraphUser_to_take).takeMoney(DebtGraphUser_to_give,money);
     }
 
     public void transaction(DebtGraphUser DebtGraphUser_to_give, DebtGraphUser DebtGraphUser_to_take, long money){
-        DebtGraphUsers.get(DebtGraphUser_to_give.getDebtGraphUserId()).giveMoney(DebtGraphUser_to_take.getDebtGraphUserId(),money);
-        DebtGraphUsers.get(DebtGraphUser_to_take.getDebtGraphUserId()).takeMoney(DebtGraphUser_to_give.getDebtGraphUserId(),money);
+        users.get(DebtGraphUser_to_give.getDebtGraphUserId()).giveMoney(DebtGraphUser_to_take.getDebtGraphUserId(),money);
+        users.get(DebtGraphUser_to_take.getDebtGraphUserId()).takeMoney(DebtGraphUser_to_give.getDebtGraphUserId(),money);
     }
 
     public boolean addDebtGraphUser(DebtGraphUser DebtGraphUser_to_add){
-        if(DebtGraphUsers.containsKey(DebtGraphUser_to_add.getDebtGraphUserId()))
+        if(users.containsKey(DebtGraphUser_to_add.getDebtGraphUserId()))
             return false;
         else{
-            DebtGraphUsers.put(DebtGraphUser_to_add.getDebtGraphUserId(),DebtGraphUser_to_add);
+            users.put(DebtGraphUser_to_add.getDebtGraphUserId(),DebtGraphUser_to_add);
             return true;
         }
     }
 
     public void Print(){
-        for(ObjectId DebtGraphUserId:DebtGraphUsers.keySet()){
-            System.out.println(DebtGraphUsers.get(DebtGraphUserId).name);
-            for(ObjectId debtorId:DebtGraphUsers.get(DebtGraphUserId).debtorList()){
+        for(ObjectId DebtGraphUserId:users.keySet()){
+            System.out.println(users.get(DebtGraphUserId).name);
+            for(ObjectId debtorId:users.get(DebtGraphUserId).debtorList()){
                 System.out.print("\t");
-                System.out.print(DebtGraphUsers.get(DebtGraphUserId).name);
+                System.out.print(users.get(DebtGraphUserId).name);
                 System.out.print(" -> ");
-                System.out.print(DebtGraphUsers.get(debtorId).name);
+                System.out.print(users.get(debtorId).name);
                 System.out.print(" : ");
-                System.out.print(DebtGraphUsers.get(DebtGraphUserId).debt_amount(debtorId));
+                System.out.print(users.get(DebtGraphUserId).debt_amount(debtorId));
                 System.out.println();
             }
         }
@@ -55,8 +55,8 @@ public class DebtGraph {
 
     private HashMap<ObjectId,DebtGraphUser> copy_debts(){
         HashMap<ObjectId,DebtGraphUser> new_debts = new HashMap<>();
-        for(ObjectId DebtGraphUserId : DebtGraphUsers.keySet()){
-            new_debts.put(DebtGraphUserId,new DebtGraphUser(DebtGraphUsers.get(DebtGraphUserId)));
+        for(ObjectId DebtGraphUserId : users.keySet()){
+            new_debts.put(DebtGraphUserId,new DebtGraphUser(users.get(DebtGraphUserId)));
         }
         return new_debts;
     }
@@ -66,8 +66,8 @@ public class DebtGraph {
             boolean flag=false;
             ObjectId id1=new ObjectId(),id2=new ObjectId(),id3=new ObjectId();
             long money=0;
-            Set<ObjectId> all_DebtGraphUsers = new_debts.keySet();
-            for(ObjectId DebtGraphUserId1: all_DebtGraphUsers){
+            Set<ObjectId> all_users = new_debts.keySet();
+            for(ObjectId DebtGraphUserId1: all_users){
                 if (flag)
                         break;
                 Set<ObjectId> DebtGraphUser1debtors = new_debts.get(DebtGraphUserId1).debtorList();
@@ -99,22 +99,22 @@ public class DebtGraph {
             }
         }
         DebtGraph tmp = new DebtGraph();
-        tmp.DebtGraphUsers = (new_debts);
+        tmp.users = (new_debts);
         return tmp;
     }
 
     public String toStr(){
         StringBuilder result = new StringBuilder();
-        for(ObjectId DebtGraphUserId:DebtGraphUsers.keySet()){
-            result.append(DebtGraphUsers.get(DebtGraphUserId).name);
+        for(ObjectId DebtGraphUserId:users.keySet()){
+            result.append(users.get(DebtGraphUserId).name);
             result.append("\n");
-            for(ObjectId debtorId:DebtGraphUsers.get(DebtGraphUserId).debtorList()){
+            for(ObjectId debtorId:users.get(DebtGraphUserId).debtorList()){
                 result.append("|");
-                result.append(DebtGraphUsers.get(DebtGraphUserId).name);
+                result.append(users.get(DebtGraphUserId).name);
                 result.append(" -> ");
-                result.append(DebtGraphUsers.get(debtorId).name);
+                result.append(users.get(debtorId).name);
                 result.append(" : ");
-                result.append(DebtGraphUsers.get(DebtGraphUserId).debt_amount(debtorId));
+                result.append(users.get(DebtGraphUserId).debt_amount(debtorId));
                 result.append("|");
             }
         }

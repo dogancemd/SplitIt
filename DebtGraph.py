d3a0b3c5046091a4d5a67bbee69db3bd46fc83d2
dogancemd <88824390+dogancemd@users.noreply.github.com>
2024-01-03 21:57:14 +0300

    Changed simplification algorithm

--- DebtGraph.py
+++ DebtGraph.py
@@ -1,5 +1,7 @@
+import json
+from Queue import Queue
 class DebtGraph:
-    def __init__(self, users: list = None, debts: dict = None) -> None:
+    def __init__(self, users: list = None, debts: dict = None,name : str = None) -> None:
         if users is not None:
             self.users: list = users
         else:
@@ -8,6 +10,7 @@ class DebtGraph:
             self.debts: dict = debts
         else:
             self.debts: dict = dict()
+        self.name: str = name
 
     def add_user(self, user) -> bool:
         if user not in self.users:
@@ -17,62 +20,101 @@ class DebtGraph:
             print('User is already in')
             return False
 
-    def add_debt(self, debtee, debtor, debt, users=None, debts=None) -> bool:
+    def add_debt(self, debtees, debtor, debt, users=None, debts=None) -> bool:
+        if(type(debtees) == list):
+            newdebt = debt/(len(debtees))
+            for debtee in debtees:
+                if (debtee != debtor):
+                    if not self.add_debt_helper(debtee, debtor, newdebt, users, debts):
+                        return False
+        else:
+            return self.add_debt_helper(debtees, debtor, debt, users, debts)
+                
+
+    def add_debt_helper(self, debtee, debtor, debt, users=None, debts=None) -> bool:
         if users is None:
             users = self.users
         if debts is None:
             debts = self.debts
+        result = False
         if debtee in users and debtor in users and debtor != debtee:
             if debtee not in debts.keys():
                 debts[debtee] = list()
             # Check if debtee has already debt to debtor
             for i in range(len(debts[debtee])):
+                if result:
+                    break
                 debtor_in_list, debt_in_list = debts[debtee][i]
                 if debtor_in_list == debtor:
                     debts[debtee][i] = (debtor, debt_in_list + debt)
-                    return True
+                    result = True
             # If not, check if debtor has previous debt to debtee
-            if debtor in debts.keys():
-                for i in range(len(debts[debtor])):
-                    debtor_in_list, debt_in_list = debts[debtor][i]
-                    if debtor_in_list == debtee:
-                        if debt_in_list > debt:
-                            debts[debtor][i] = (debtee, debt_in_list - debt)
-                        elif debt > debt_in_list:
-                            debt -= debt_in_list
-                            debts[debtor].pop(i)
-                            debts[debtee].append((debtor, debt))
-                        else:
-                            debts[debtor].pop(i)
-                        return True
+            if not result:
+                if debtor in debts.keys():
+                    for i in range(len(debts[debtor])):
+                        if result:
+                            break
+                        debtor_in_list, debt_in_list = debts[debtor][i]
+                        if debtor_in_list == debtee:
+                            if debt_in_list > debt:
+                                debts[debtor][i] = (debtee, debt_in_list - debt)
+                            elif debt > debt_in_list:
+                                debt -= debt_in_list
+                                debts[debtor].pop(i)
+                                debts[debtee].append((debtor, debt))
+                            else:
+                                debts[debtor].pop(i)
+                            result = True
             # If debtor has no previous debt to debtee, create new debt
-            debts[debtee].append((debtor, debt))
-            return True
-
+            if not result:
+                debts[debtee].append((debtor, debt))
+                result = True
+            if(debtor in debts.keys() and len(debts[debtor]) == 0):
+                debts.pop(debtor)
+            if(debtee in debts.keys() and len(debts[debtee]) == 0):
+                debts.pop(debtee)
+            return result
+    def get_users(self):
+        return self.users
     def simplify_debts(self):
         users = list(self.users)
         debts = dict(self.debts)
-        debtees = debts.keys()
         while True:
             flag = False
-            for user1 in debtees:
+            debtees = list(debts.keys())
+            for debtee in debtees:
                 if flag:
-                    continue
-                for user2, debt1 in debts[user1]:
+                    break
+                parent = dict()
+                first_debtors = list(map(lambda x: x[0], debts[debtee]))
+                debtors_queue = Queue()
+                for debtor in first_debtors:
+                    debtors_queue.enqueue(debtor)
+                    parent[debtor] = debtee
+                while not debtors_queue.isEmpty():
                     if flag:
                         break
-                    if user2 in debtees:
-                        for user3, debt2 in debts[user2]:
-                            if flag:
-                                break
-                            if user3 in [debtor for debtor, debt in debts[user1]]:
-                                self.add_debt(user3, user2, debt2, users, debts)
-                                self.add_debt(user2, user1, debt2, users, debts)
-                                self.add_debt(user1, user3, debt2, users, debts)
+                    debtor = debtors_queue.dequeue()
+                    if debtor in debts.keys():
+                        for debtee2, debt in debts[debtor]:
+                            if debtee2 in first_debtors:
                                 flag = True
+                                tmp_debtee = debtor
+                                tmp_debtor = debtee2
+                                debt = debts[debtee][first_debtors.index(tmp_debtor)][1]
+                                self.add_debt(tmp_debtor, debtee, debt)
+                                while tmp_debtee != debtee :
+                                    self.add_debt(tmp_debtee, tmp_debtor, debt)
+                                    tmp_debtor = tmp_debtee
+                                    tmp_debtee = parent[tmp_debtee]
+                                self.add_debt(tmp_debtee, tmp_debtor, debt)
                                 break
+                            else:
+                                if debtee2 not in parent.keys():
+                                    debtors_queue.enqueue(debtee2)
+                                    parent[debtee2] = debtor
             if not flag:
-                break
+                break    
         return DebtGraph(users, debts)
 
     def __str__(self) -> str:
@@ -85,3 +127,7 @@ class DebtGraph:
 
     def __repr__(self) -> str:
         return str(self)
+
+    def save(self, file_name : str):
+        with open(file_name, 'w') as f:
+            json.dump(self.debts, f)
